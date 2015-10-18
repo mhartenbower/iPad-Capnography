10efe4da9a9ab61f47c37519b7ef7aac918e22d5
Johnathan Cornella <jcornella11@hotmail.com>
2015-10-17 21:44:21 -0700

    additional work done on storyboard. working on creating a custom table view

---
+++ TextInputCell.h
@@ -0,0 +1,13 @@
+//
+//  TextInputCell.h
+//  Capstone2
+//
+//  Created by Johnathan Cornella on 10/17/15.
+//  Copyright © 2015 Johnathan Cornella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TextInputCell : UITableViewCell
+
+@end
---
+++ TextInputCell.m
@@ -0,0 +1,13 @@
+//
+//  TextInputCell.m
+//  Capstone2
+//
+//  Created by Johnathan Cornella on 10/17/15.
+//  Copyright © 2015 Johnathan Cornella. All rights reserved.
+//
+
+#import "TextInputCell.h"
+
+@implementation TextInputCell
+
+@end
