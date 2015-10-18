ca859c434b42f6c7ecd38a68b8018a37514fb324
Johnathan Cornella <jcornella11@hotmail.com>
2015-10-18 00:47:34 -0700

    Additional Updates
    -Made more fixes and updates lol

---
+++ NewPatientInformation.h
@@ -0,0 +1,13 @@
+//
+//  NewPatientInformation.h
+//  Capstone2
+//
+//  Created by Johnathan Cornella on 10/18/15.
+//  Copyright © 2015 Johnathan Cornella. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewPatientInformation : UITableViewController
+
+@end
---
+++ NewPatientInformation.m
@@ -0,0 +1,13 @@
+//
+//  NewPatientInformation.m
+//  Capstone2
+//
+//  Created by Johnathan Cornella on 10/18/15.
+//  Copyright © 2015 Johnathan Cornella. All rights reserved.
+//
+
+#import "NewPatientInformation.h"
+
+@implementation NewPatientInformation
+
+@end
